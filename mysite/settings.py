93320e4e203eec2605f4f9b90f24f8f352bc8879
michal <m.rzeszotarski45@gmail.com>
2021-08-26 23:13:51 +0200

    settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['orzech45.pythonanywhere.com']
 
 
 # Application definition
