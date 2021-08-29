6131ea9553d56d3467977d8beca9289298ce52ca
orzech45 <m.rzeszotarski45@gmail.com>
2021-08-30 00:23:59 +0200

    PC stacjonarny w allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['orzech45.pythonanywhere.com']
+ALLOWED_HOSTS = ['orzech45.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
